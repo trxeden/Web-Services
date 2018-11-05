7815b06833a7a234ae9e7867f23ad55ddbc2f1b4
trxeden <44758704+trxeden@users.noreply.github.com>
2018-11-04 21:00:02 -0500

    Added Home controller

---
+++ PlantPlacesWebService/PlantPlacesWebService/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlantPlacesWebService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+        // GET api/home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
